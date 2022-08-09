88afef02f3f6b5ec3787d785f6a894e88cd48351
Allan Colibao <colibaoallanreyes@gmail.com>
2022-08-09 16:18:07 +0800

    Day 2

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CandidatesList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CandidatesList from './components/CandidatesList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CandidatesList
   }
 }
 </script>
 
 <style>
+  body {
+    background-color: #fcfcfc
+  }
+
+  .container {
+    background-color: #232939;
+    width: 530px;
+    margin: auto;
+    padding: 10px;
+    border-radius: 10px;
+    box-shadow: #f8f8f8 2px 2px 3px;
+  }
+
+  .candidate-container {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .candidate-card {
+    background-color: #ffffff;
+    width: auto;
+    margin: 8px 8px;
+    padding: 10px;
+    border-radius: 10px;
+    box-shadow: #5e5e5e 1px 1px 8px;
+  }
+
+  .candidate-count {
+    color: #e9e9e9;
+  }
+
+  .title {
+    color: #e9e9e9;
+  }
+
+  .error-badge {
+    margin: 5px 0px;
+    color: #ff4551;
+  }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
